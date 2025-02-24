1101b3537ba73982f85d3e2037268ade120107c3
GiGi Hurtault <gigi@hurtault.dev>
2025-02-23 20:12:52 -0500

    make message optional

--- camelsay/bin/main.ml
+++ camelsay/bin/main.ml
@@ -1,16 +1,16 @@
 open Core
 
-(* TODO add flag for different camel styles *)
-
-let camel_speech ?(message = "*spits*") () =
+let camel_speech ~message =
   let camel_body =
     {|
-      //
-  _oo\
-  (__/ \  _  _
+  \
+   \
+     //
+   _oo\
+  (__/ \ _  _
     \  \/ \/ \
     (         )\
-      \_______/  \
+     \_______/  \
       [[] [[]
       [[] [[]
     |}
@@ -18,15 +18,16 @@ let camel_speech ?(message = "*spits*") () =
   let full_text = (Printf.sprintf "\n< %s >\n" message) ^ camel_body in
   print_endline full_text 
 
-let message_param =
-  (* makes sense for this to be anonymous. it's the only flag (for now), and it's the main thing one would pass in *)
-  let open Command.Param in
-  anon ("message" %: string)
+let get_message = function
+| Some m -> m
+| None -> "*spits*"
 
 let camelsay_command =
   Command.basic
   ~summary:"Forces the captive camel to utter a phrase of your choosing."
-  (Command.Param.map message_param ~f:(fun message () ->
-    camel_speech ~message ()))
+  (let%map_open.Command message =
+    anon (maybe ("message" %: string))
+  in 
+  fun () -> camel_speech ~message:(get_message message))
 
 let () = Command_unix.run ~version:"0.01" camelsay_command
