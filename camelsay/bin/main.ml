15255b7bc8a31d20ac203036d62045f08b0733a9
GiGi Hurtault <gigi@hurtault.dev>
2025-07-12 01:22:34 -0400

    add emoji flag

--- camelsay/bin/main.ml
+++ camelsay/bin/main.ml
@@ -1,7 +1,10 @@
 open Core
 
-let camel_speech ~message =
-  let camel_body =
+let render_output ~message ~body_type =
+  (* idk how to get unicode actually rendering so this is here as a regular string for now. doubt this works *)
+  let emoji_camel = "U+1F42B"
+  in
+  let ascii_camel_body =
     {|
   \
    \
@@ -15,19 +18,33 @@ let camel_speech ~message =
       [[] [[]
     |}
   in
-  let full_text = (Printf.sprintf "\n< %s >\n" message) ^ camel_body in
-  print_endline full_text 
+  let camel_body =
+    match body_type with
+    | `Emoji -> emoji_camel
+    | `Ascii -> ascii_camel_body
+  in
+  let camel_and_speech = (Printf.sprintf "\n< %s >\n" message) ^ (camel_body) in
+  print_endline camel_and_speech
 
-let get_message = function
+let get_message = 
+  function
 | Some m -> m
 | None -> "*spits*"
+;;
+
+let get_camel_type emoji_flag = if emoji_flag then `Emoji else `Ascii;;
 
 let camelsay_command =
   Command.basic
-  ~summary:"Forces the captive camel to utter a phrase of your choosing."
-  (let%map_open.Command message =
-    anon (maybe ("message" %: string))
-  in 
-  fun () -> camel_speech ~message:(get_message message))
+  ~summary:"Forces the camel to utter a phrase of your choosing."
+  [%map_open.Command
+    let message = anon (maybe ("message" %: string))
+    and
+    emoji = flag "emoji" Command.Flag.no_arg ~doc:"Display an emoji camel instead of the default ascii camel"
+    in 
+    (* get_camel_type should return one of the variants that I defined in camel_body *)
+    fun () -> render_output ~message:(get_message message) ~body_type:(get_camel_type emoji)
+   ]
+    
 
 let () = Command_unix.run ~version:"0.01" camelsay_command
