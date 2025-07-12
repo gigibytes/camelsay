39562b937a5196c61f32190ba1c6ab44b8ba8ea9
GiGi Hurtault <gigi@hurtault.dev>
2025-07-12 02:11:08 -0400

    add fake translation

--- camelsay/bin/main.ml
+++ camelsay/bin/main.ml
@@ -27,13 +27,43 @@ let render_output ~message ~body_type =
   print_endline camel_and_speech
 ;;
 
-let get_message = 
-  function
-| Some m -> m
-| None -> "*spits*"
-;;
 
 let get_camel_type emoji_flag = if emoji_flag then `Emoji else `Ascii;;
+
+let get_translation ~message ~language_code = 
+(* this is where we reach out to whatever API i'm using for this *)
+ if (Option.is_some language_code) then
+    message ^ "this message has been translated :)"
+  else message
+;;
+
+let translate ~message ~language =
+  let language_code =
+    (* TODO: support non roman chars and accents with unicode *)
+    match language with
+    | Some l when String.Caseless.equal l "es" -> "es_SP"
+    | None | _ -> "no_op"
+  in
+  let translation_note =
+    match language_code with
+    | "no_op" -> "No translation performed, language not recognized"
+    | lang -> [%string "Translated message to %{lang}"]
+  in
+  let maybe_translated_message =
+    if (not (String.equal language_code "no_op")) then (get_translation ~message ~language_code) else message
+  in
+  maybe_translated_message ^ translation_note
+;;
+
+let get_message ~message ~translation_target_lang = 
+  let default_message = "*spits*"
+  in
+  match message with
+  | Some m when (Option.is_some translation_target_lang) -> translate ~message:m ~language:translation_target_lang
+  (* Don't translate when the translation_target_lang option is None/when guard eq false *)
+  | Some m -> m
+  | None -> default_message
+;;
 
 let camelsay_command =
   Command.basic
@@ -42,10 +72,12 @@ let camelsay_command =
     let message = anon (maybe ("message" %: string))
     and
     emoji = flag "emoji" Command.Flag.no_arg ~doc:"Display an emoji camel instead of the default ascii camel"
+    and
+    (* Should offer a set of options to start *)
+    translation_target_lang = flag "translate" (optional string) ~doc:"Translate to a different language"
     in 
     (* get_camel_type should return one of the variants that I defined in camel_body *)
-    fun () -> render_output ~message:(get_message message) ~body_type:(get_camel_type emoji)
+    fun () -> render_output ~message:(get_message ~message ~translation_target_lang) ~body_type:(get_camel_type emoji)
    ]
     
-
-let () = Command_unix.run ~version:"0.01" camelsay_command
+let () = Command_unix.run ~version:"0.1" camelsay_command
